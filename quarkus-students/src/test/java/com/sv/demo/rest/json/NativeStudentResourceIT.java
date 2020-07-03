6c9f2e104c20220bc3cb842590d55945a85ae308
Liviu Vasut <liviu.vasut@softvision.ro>
2020-07-03 12:01:37 +0300

    generated quarkus project

---
+++ quarkus-students/src/test/java/com/sv/demo/rest/json/NativeStudentResourceIT.java
@@ -0,0 +1,9 @@
+package com.sv.demo.rest.json;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeStudentResourceIT extends StudentResourceTest {
+
+    // Execute the same tests but in native mode.
+}
---
+++ quarkus-students/src/test/java/com/sv/demo/rest/json/StudentResourceTest.java
@@ -0,0 +1,21 @@
+package com.sv.demo.rest.json;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class StudentResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/student")
+          .then()
+             .statusCode(200)
+             .body(is("hello"));
+    }
+
+}

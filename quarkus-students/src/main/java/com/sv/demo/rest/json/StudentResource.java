c59892ee37380859d2be77b2bb662d545e7c82e5
Liviu Vasut <liviu.vasut@softvision.ro>
2020-07-06 15:08:48 +0300

    retry example

--- quarkus-students/src/main/java/com/sv/demo/rest/json/StudentResource.java
+++ quarkus-students/src/main/java/com/sv/demo/rest/json/StudentResource.java
@@ -2,24 +2,31 @@ package com.sv.demo.rest.json;
 
 import com.sv.demo.dto.Student;
 import com.sv.demo.service.StudentService;
+import java.util.Random;
 import java.util.Set;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.metrics.MetricUnits;
 import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.metrics.annotation.Timed;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Path("/student")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class StudentResource {
+
+    private static final Logger LOG = LoggerFactory.getLogger(StudentResource.class);
 
     @Inject
     StudentService studentService;
@@ -34,7 +41,12 @@ public class StudentResource {
     @GET
     @Path("/{id}")
     @Counted(name = "getByIdCount", description = "How many calls to the getById endpoint.")
+    @Retry(maxRetries = 4)
     public Student getById(@PathParam("id") long id) {
+        if (new Random().nextBoolean()) {
+            LOG.info("generating an error for id " + id);
+            throw new InternalServerErrorException("Simulated failure for id " + id);
+        }
         return studentService.findById(id);
     }
 

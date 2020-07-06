9890a51cc925eec3d32423e4bc56be77a9535868
Liviu Vasut <liviu.vasut@softvision.ro>
2020-07-06 12:11:35 +0300

    student profile dto

---
+++ quarkus-student-profiles/src/main/java/com/sv/demo/dto/StudentProfile.java
@@ -0,0 +1,52 @@
+package com.sv.demo.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class StudentProfile {
+
+    @JsonProperty("login")
+    private String name;
+
+    @JsonProperty("avatar_url")
+    private String avatarUrl;
+
+    @JsonProperty("repos_url")
+    private String reposUrl;
+
+    @JsonProperty("created_at")
+    private String createdAt;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getReposUrl() {
+        return reposUrl;
+    }
+
+    public void setReposUrl(String reposUrl) {
+        this.reposUrl = reposUrl;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+}

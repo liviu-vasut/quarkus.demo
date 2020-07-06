6188ce92f1a1973b07629bb42b47a42e0542a8a0
Liviu Vasut <liviu.vasut@softvision.ro>
2020-07-06 14:33:45 +0300

    controller for student profiles

---
+++ quarkus-student-profiles/src/main/java/com/sv/demo/rest/json/StudentProfileResource.java
@@ -0,0 +1,26 @@
+package com.sv.demo.rest.json;
+
+import com.sv.demo.dto.StudentProfile;
+import com.sv.demo.service.StudentService;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/profile")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class StudentProfileResource {
+
+    @Inject
+    private StudentService studentService;
+
+    @GET
+    @Path("/{name}")
+    public StudentProfile getProfile(@PathParam("name") String name) {
+        return studentService.getProfileByName(name);
+    }
+}
